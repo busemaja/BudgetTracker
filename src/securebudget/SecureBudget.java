260828216ea4c4ace82b47db59a2cfe296684b98
Maria Jansson <mj225er@student.lnu.se>
2025-09-11 19:28:58 +0200

    Add SecureBudget and Transaction classes with initial structure

---
+++ src/securebudget/SecureBudget.java
@@ -0,0 +1,17 @@
+package securebudget;
+
+import java.util.ArrayList;
+
+/**
+ * A lightweight Java module for secure transaction management and financial analysis.
+ * 
+ * @author Maria Jansson
+ * @version 1.0.0
+ */
+public class SecureBudget {
+  private ArrayList <Transaction> transactions = new ArrayList<>();
+
+    public SecureBudget() {
+    }
+
+}
---
+++ src/securebudget/Transaction.java
@@ -0,0 +1,23 @@
+package securebudget;
+
+import java.util.Date;
+
+/**
+ * 
+ * 
+ * @author Maria Jansson
+ * @version 1.0.0
+ */
+
+class Transaction {
+  private double amount;
+  private Date date;
+  private String category;
+  private String description;
+
+  Transaction() {
+
+  }
+  
+  
+}
