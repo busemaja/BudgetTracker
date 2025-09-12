301565088cd1ce9e112d7483c5c5005c8add4a8c
Maria Jansson <mj225er@student.lnu.se>
2025-09-12 19:15:25 +0200

    Refine Transaction class constructor and update documentation

--- src/securebudget/Transaction.java
+++ src/securebudget/Transaction.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 /**
  * A class representing a single transaction.
+ * Consists of amount, date, category, and an optional description of the transaction.
  * 
  * @author Maria Jansson
  * @version 1.0.0
@@ -18,7 +19,12 @@ class Transaction {
   /**
    * Constructor.
    */
-  Transaction() {}
+  Transaction(double amount, String category, String description) {
+    this.amount = amount;
+    this.date = new Date();
+    this.category = category;
+    this.description = description;
+  }
   
   
 }
