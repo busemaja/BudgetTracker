b8e650c9cd382a690924b600f63bd8fde4fe6d9b
Maria Jansson <mj225er@student.lnu.se>
2025-09-17 13:18:30 +0200

    Add AuditLog class to represent transaction log posts

---
+++ src/securebudget/AuditLog.java
@@ -0,0 +1,15 @@
+package securebudget;
+
+/**
+ * A class representing a single log post for a transaction.
+ * 
+ * @author Maria Jansson
+ * @version 1.0.0
+ */
+
+public class AuditLog {
+  AuditLog() {
+    
+  }
+  
+}
