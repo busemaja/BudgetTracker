46e4f469a1b02cad5fd8fcf646a019c87f63b9c2
Maria Jansson <mj225er@student.lnu.se>
2025-09-24 19:18:19 +0200

    Update documentation for TransactionCategories class

--- src/budgettracker/TransactionCategories.java
+++ src/budgettracker/TransactionCategories.java
@@ -1,5 +1,9 @@
 package budgettracker;
 
+/**
+ * A class containing the categories used in the module. Feel free to adapt by need.
+ * @author Maria Jansson
+ */
 public enum TransactionCategories {
   FOOD, HOUSING, UTILITIES, TRANSPORTATION, INSURANCE, HOUSEHOLD, PERSONAL, ENTERTAINMENT, SAVINGS
 }
