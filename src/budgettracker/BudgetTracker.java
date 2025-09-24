0ff60a3f8e5b94eaf2b7ad5ebe906e9479c82c3a
Maria Jansson <mj225er@student.lnu.se>
2025-09-24 19:13:04 +0200

    Enhance documentation for public transaction management methods in BudgetTracker

--- src/budgettracker/BudgetTracker.java
+++ src/budgettracker/BudgetTracker.java
@@ -16,6 +16,12 @@ public class BudgetTracker {
 
   public BudgetTracker() {}
 
+  /**
+   * Adds a transaction to the active list of transactions, and also adds it to the log.
+   * @param transactionName - a descriptive name, for example "Coffee" or "Petrol".
+   * @param amount - primitive type double
+   * @param category - found in the class TransactionCategories, change there if needed.
+   */
   public void addTransactionAndLogIt(String transactionName, double amount, TransactionCategories category) {
     // TODO: add validation of input?
     Transaction transaction = new Transaction(transactionName, amount, category);
@@ -23,6 +29,10 @@ public class BudgetTracker {
     logTransaction(transaction);
   }
 
+  /**
+   * Removes a transaction from the active list, and adds the removal to the log.
+   * @param transactionId - the current transaction's unique ID
+   */
   public void removeTransactionAndLogIt(int transactionId) {
     try {
       Transaction transaction = getTransaction(transactionId);
@@ -34,6 +44,10 @@ public class BudgetTracker {
     }
   }
 
+  /**
+   * Returns the total sum of all transactions in the active list.
+   * @return - the sum as a double
+   */
   public double getCurrentTotal() {
     double sum = 0;
     for (Transaction transaction : transactions) {
@@ -42,6 +56,11 @@ public class BudgetTracker {
     return sum;
   }
 
+  /**
+   * Returns the total sum of all transactions of a certain category in the active list.
+   * @param category - found in the class TransactionCategories
+   * @return - the sum as a double
+   */
   public double getCurrentTotalByCategory(TransactionCategories category) {
     double sum = 0;
     for (Transaction transaction : transactions) {
@@ -52,6 +71,10 @@ public class BudgetTracker {
     return sum;
   }
 
+  /**
+   * Returns the transaction whith the largest amount in the active list.
+   * @return - the transaction object
+   */
   public Transaction getLargestListedTransaction() {
     ArrayList<Transaction> transactionsCopy = new ArrayList<>(transactions);
     transactionsCopy.sort((transaction1, transaction2) -> 
