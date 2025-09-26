4df3b6a7e8c1fe75b7a71f38efbd142281db9d27
Maria Jansson <mj225er@student.lnu.se>
2025-09-26 18:09:36 +0200

    Add toFormattedString method to format output. Add category as a field in the class, also make all fields final

--- src/budgettracker/LogEntry.java
+++ src/budgettracker/LogEntry.java
@@ -1,6 +1,7 @@
 package budgettracker;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * A class representing a single log post for a transaction.
@@ -8,29 +9,40 @@ import java.time.LocalDateTime;
  * @author Maria Jansson
  */
 
-public class LogEntry {
-  private LocalDateTime timestamp;
-  private Action action;
-  private int transactionId;
-  private String name;
-  private double amount;
+class LogEntry {
+  private final LocalDateTime timestamp;
+  private final Action action;
+  private final int transactionId;
+  private final TransactionCategories category;
+  private final String name;
+  private final double amount;
 
-  /**
-   * The only allowed actions for a log entry is ADD and REMOVE at this point.
-   */
-  public enum Action {
+  // Add actions to your heart's content, but don't forget to add and implement methods in BudgetTracker.
+  enum Action {
     ADD, 
     REMOVE
   }
 
-  LogEntry(Action action, int transactionId, String name, double amount) {
+  LogEntry(Action action, TransactionCategories category, int transactionId, String name, double amount) {
     this.timestamp = LocalDateTime.now();
     this.action = action;
+    this.category = category;
     this.transactionId = transactionId;
     this.name = name;
     this.amount = amount;
   }
 
-  // TODO: lägg till toString()
+    // Example of return string (values separeted by tab): ADD	25	2025-09-25 15:26	FOOD	Fika	125,00kr
+  public String toFormattedString() {
+    DateTimeFormatter wantedDateTimeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    String formattedTimestamp = this.timestamp.format(wantedDateTimeFormat);
+    return this.action + "\t" 
+      + this.transactionId + "\t" 
+      + formattedTimestamp + "\t" 
+      + this.category + "\t" 
+      + this.name + "\t" 
+      + String.format("%.2f", this.amount) 
+      + "kr";
+  }
   
 }
