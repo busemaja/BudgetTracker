3ee8d030fb41683a2edb7077732471e5c866e8c5
Maria Jansson <mj225er@student.lnu.se>
2025-09-26 18:26:18 +0200

    Add public modifier to TransactionCategories enum for accessibility

--- src/budgettracker/TransactionCategories.java
+++ src/budgettracker/TransactionCategories.java
@@ -4,6 +4,6 @@ package budgettracker;
  * A file containing the categories used in the module. Feel free to adapt by need.
  * @author Maria Jansson
  */
-enum TransactionCategories {
+public enum TransactionCategories {
   FOOD, HOUSING, UTILITIES, TRANSPORTATION, INSURANCE, HOUSEHOLD, PERSONAL, ENTERTAINMENT, SAVINGS
 }
